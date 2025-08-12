0550dd110cff02e3ea073159b69acadb1db4965e
pedroholguin <dropeholguin1994@gmail.com>
2025-08-12 13:53:28 -0500

    Initial app

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import MenuItems from "./components/MenuItems";
+import OrderContents from "./components/OrderContents";
+import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
+
+function App() {
+  const { addItem, order, removeItem } = useOrder();
+
+  return (
+    <>
+      <header className="bg-teal-400 text-white py-5">
+        <h1 className="text-center text-4xl font-bold">
+          Calculadora de Propinas
+        </h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto p-5 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-bold mb-4">Menú</h2>
+
+          <div className="space-y-3">
+            {menuItems.map((item) => (
+              <MenuItems key={item.id} item={item} addItem={addItem} />
+            ))}
+          </div>
+        </div>
+
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents order={order} removeItem={removeItem} />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
